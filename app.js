701bced51eeb03d2f6ebd795be188f8615c2940b
christensenjoshua <christensen.joshua@outlook.com>
2018-07-11 20:21:30 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var david ={
+    name: "David",
+    age: 67,
+    occupation: "Electrician"
+}
+var paula ={
+    name: "Paula",
+    age: 68,
+    occupation: "Office Manager"
+}
+var me ={
+    name: "Joshua",
+    age: "28",
+    siblings: [
+        {name:"Kimberly",
+        relation: "sister",
+        age: "31"
+        },
+        {name:"Christopher",
+        relation: "brother",
+        age: "32"
+        }
+    ],
+    occupation: "Programmer",
+    father: david,
+    mother: paula,
+}
+console.log("My name is "+me.name+". I am "+me.age+" years old and I am a "+me.occupation+".")
+console.log("My mother "+me.mother.name+" is "+me.mother.age+" years old and is an "+me.mother.occupation+".")
+console.log("My father "+me.father.name+" is "+me.father.age+" years old and is an "+me.father.occupation+".")
+var numSiblings = me.siblings.length - 1
+console.log("I have "+numSiblings+" siblings.")
+siblingIndex = 0
+if(numSiblings > 0) {
+    do{
+        if(siblingIndex > 0){
+            console.log("and")
+        }
+        console.log(me.siblings[siblingIndex].name+" is my "+me.siblings[siblingIndex].relation+" and is "+me.siblings[siblingIndex].age+" years old.")
+        siblingIndex++
+    }while(siblingIndex <= numSiblings)
+} else {
+    console.log("Just kidding, I don't have any siblings.")
+}
+function checkDate(){
+    alert("Check the console log, this doesn't do anything anymore!")
+}
