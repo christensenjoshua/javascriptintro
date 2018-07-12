9bc13a7737214ac786e377b67945b273dfcb856c
christensenjoshua <christensen.joshua@outlook.com>
2018-07-11 20:35:47 -0600

    final formatting

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-var david ={
+var david = {
     name: "David",
     age: 67,
     occupation: "Electrician"
 }
-var paula ={
+var paula = {
     name: "Paula",
     age: 68,
     occupation: "Office Manager"
 }
-var me ={
+var me = {
     name: "Joshua",
     age: "28",
     siblings: [
-        {name:"Kimberly",
-        relation: "sister",
-        age: "31"
+        {
+            name: "Kimberly",
+            relation: "sister",
+            age: "31"
         },
-        {name:"Christopher",
-        relation: "brother",
-        age: "32"
+        {
+            name: "Christopher",
+            relation: "brother",
+            age: "32"
         }
     ],
     occupation: "Programmer",
@@ -27,26 +29,26 @@ var me ={
 }
 var element = document.getElementById("myStory")
 
-element.innerText +="My name is "+me.name+". I am "+me.age+" years old and I am a "+me.occupation+"."
-element.innerText +="My mother "+me.mother.name+" is "+me.mother.age+" years old and is an "+me.mother.occupation+"."
-element.innerText +="My father "+me.father.name+" is "+me.father.age+" years old and is an "+me.father.occupation+"."
+element.innerText += "My name is " + me.name + ". I am " + me.age + " years old and I am a " + me.occupation + "."
+element.innerText += "My mother " + me.mother.name + " is " + me.mother.age + " years old and is an " + me.mother.occupation + "."
+element.innerText += "My father " + me.father.name + " is " + me.father.age + " years old and is an " + me.father.occupation + "."
 var numSiblings = me.siblings.length - 1
-element.innerText +="I have "+numSiblings+" siblings."
+element.innerText += "I have " + numSiblings + " siblings."
 // Lie about my number of siblings
 numSiblings = 0
 //
 siblingIndex = 0
-if(numSiblings > 0) {
-    do{
-        if(siblingIndex > 0){
-            element.innerText +="and"
+if (numSiblings > 0) {
+    do {
+        if (siblingIndex > 0) {
+            element.innerText += "and"
         }
-        element.innerText +=me.siblings[siblingIndex].name+" is my "+me.siblings[siblingIndex].relation+" and is "+me.siblings[siblingIndex].age+" years old."
+        element.innerText += me.siblings[siblingIndex].name + " is my " + me.siblings[siblingIndex].relation + " and is " + me.siblings[siblingIndex].age + " years old."
         siblingIndex++
-    }while(siblingIndex <= numSiblings)
+    } while (siblingIndex <= numSiblings)
 } else {
-    element.innerText +="Just kidding, I don't have any siblings."
+    element.innerText += "Just kidding, I don't have any siblings."
 }
-function checkDate(garbage, moreGarbage){
+function checkDate(garbage, moreGarbage) {
     alert("I don't do anything anymore!")
 }
