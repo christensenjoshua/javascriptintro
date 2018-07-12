0f83e98189ac1f0ba108043b75b45ad2d2b3c6b9
christensenjoshua <christensen.joshua@outlook.com>
2018-07-11 20:32:54 -0600

    changed console log to now write text to a paragraph

--- app.js
+++ app.js
@@ -25,23 +25,25 @@ var me ={
     father: david,
     mother: paula,
 }
-console.log("My name is "+me.name+". I am "+me.age+" years old and I am a "+me.occupation+".")
-console.log("My mother "+me.mother.name+" is "+me.mother.age+" years old and is an "+me.mother.occupation+".")
-console.log("My father "+me.father.name+" is "+me.father.age+" years old and is an "+me.father.occupation+".")
+var element = document.getElementById("myStory")
+
+element.innerText +="My name is "+me.name+". I am "+me.age+" years old and I am a "+me.occupation+"."
+element.innerText +="My mother "+me.mother.name+" is "+me.mother.age+" years old and is an "+me.mother.occupation+"."
+element.innerText +="My father "+me.father.name+" is "+me.father.age+" years old and is an "+me.father.occupation+"."
 var numSiblings = me.siblings.length - 1
-console.log("I have "+numSiblings+" siblings.")
+element.innerText +="I have "+numSiblings+" siblings."
 siblingIndex = 0
 if(numSiblings > 0) {
     do{
         if(siblingIndex > 0){
-            console.log("and")
+            element.innerText +="and"
         }
-        console.log(me.siblings[siblingIndex].name+" is my "+me.siblings[siblingIndex].relation+" and is "+me.siblings[siblingIndex].age+" years old.")
+        element.innerText +=me.siblings[siblingIndex].name+" is my "+me.siblings[siblingIndex].relation+" and is "+me.siblings[siblingIndex].age+" years old."
         siblingIndex++
     }while(siblingIndex <= numSiblings)
 } else {
-    console.log("Just kidding, I don't have any siblings.")
+    element.innerText +="Just kidding, I don't have any siblings."
 }
-function checkDate(){
-    alert("Check the console log, this doesn't do anything anymore!")
+function checkDate(garbage, moreGarbage){
+    alert("I don't do anything anymore!")
 }
